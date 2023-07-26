23c4af69e3552a1d7e584a9f10d0cbe421646b82
thiago alves <thiago87gsa@gmail.com>
2023-07-26 16:27:36 -0300

    fix for

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // Minha solução -- Melhor sulução
 function solution(matrix) {
     let soma = 0
-    for (let i = 0, soma = 0; i < matrix[0].length; i++) {
+    for (let i = 0; i < matrix[0].length; i++) {
         for (let j of matrix) {
             if (j[i] === 0) break
             soma += j[i]
